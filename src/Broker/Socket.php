1a8a5dd7a0e4916755f8325e2f60fe9c54bdf69d
lazos <lajos.bencz@gmail.com>
2016-08-03 18:20:18 +0200

    progress

--- src/Broker/Socket.php
+++ src/Broker/Socket.php
@@ -33,10 +33,25 @@ class Socket extends Broker
         if(isset($url['query'])) {
             $path.= '?'.$url['query'];
         }
-        $fp = fsockopen($host, $port, $errno, $errstr);
+        $fp = fsockopen($proto.'://'.$host, $port, $errno, $errstr);
         if($fp === false) {
             throw new Exception($errno, $errstr);
         }
-
+        fputs($fp, "POST $path HTTP/1.1\r\n");
+        fputs($fp, "Host: $host\r\n");
+        fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
+        fputs($fp, "Content-length: ". strlen($data) ."\r\n");
+        fputs($fp, "Connection: close\r\n\r\n");
+        fputs($fp, $data);
+        $result = '';
+        while(!feof($fp)) {
+            // receive the results of the request
+            $result .= fgets($fp, 128);
+        }
+        fclose($fp);
+        $result = explode("\r\n\r\n", $result, 2);
+        $header = isset($result[0]) ? $result[0] : '';
+        $content = isset($result[1]) ? $result[1] : '';
+        return $content;
     }
 }
--- test/BrokerTest.php
+++ test/BrokerTest.php
@@ -11,8 +11,6 @@ class BrokerTest extends \PHPUnit_Framework_TestCase
         return [
             ['http://lazos.me/foo',[],[]],
             ['https://lazos.me/foo',[],[]],
-            ['http://lazos.me:8080/foo',[],[]],
-            ['https://lazos.me:8080/foo',[],[]],
             ['http://lazos.me/foo?foo=bar',['bop'=>'plop'],[]],
             ['https://lazos.me/foo?foo=bar',['foo'=>'baz','bop'=>'plop'],[]],
         ];
